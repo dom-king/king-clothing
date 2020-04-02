dc4e485751072c4d07fb3a0f45216228dea6566a
Dom King <dminicking91@gmail.com>
2020-04-02 12:30:24 +0100

    Updated the firebase utilities and app to recognise sign in from google account

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,29 @@ const config = {
     appId: "1:1031057462383:web:469ba2947ad909b514a99a"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName, email, createdAt, ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message); 
+        }
+    }
+
+    // May need the user ref elsewhere in the appsication so return it
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth(); 
